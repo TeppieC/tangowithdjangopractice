5f9b3a076dcfd4841ec8aed3048fdbbce10be10f
TeppieC <teppie.c@hotmail.com>
2016-07-04 00:04:30 +0800

    finished chap20

--- rango/tests.py
+++ rango/tests.py
@@ -22,7 +22,30 @@ def test_slug_line_creation(self):
     cat.save()
     self.assertEqual(cat.slug, 'random-category-string')
 
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
 class IndexViewTests(TestCase):
+    def test_index_view_with_categories(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+
+        add_cat('test',1,1)
+        add_cat('temp',1,1)
+        add_cat('tmp',1,1)
+        add_cat('tmp test temp',1,1)
+
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+
+        num_cats =len(response.context['categories'])
+        self.assertEqual(num_cats , 4)
 
     def test_index_view_with_no_categories(self):
         """
@@ -31,28 +54,4 @@ class IndexViewTests(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "There are no categories present.")
-        self.assertQuerysetEqual(response.context['categories'], []
-
-    def test_index_view_with_categories(self):
-	    """
-	    If no questions exist, an appropriate message should be displayed.
-	    """
-
-	    add_cat('test',1,1)
-	    add_cat('temp',1,1)
-	    add_cat('tmp',1,1)
-	    add_cat('tmp test temp',1,1)
-
-	    response = self.client.get(reverse('index'))
-	    self.assertEqual(response.status_code, 200)
-	    self.assertContains(response, "tmp test temp")
-
-	    num_cats =len(response.context['categories'])
-	    self.assertEqual(num_cats , 4)
-
-def add_cat(name, views, likes):
-	c = Category.objects.get_or_create(name=name)[0]
-	c.views = views
-	c.likes = likes
-	c.save()
-	return c
+        self.assertQuerysetEqual(response.context['categories'], [])
